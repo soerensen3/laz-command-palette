88613b7f07694f14d76a5f368ab286c2c8938778
soerensen3 <johannes.soerensen@gmail.com>
2019-12-07 12:47:33 +0100

    added license, description and video

---
+++ readme.md
@@ -0,0 +1,26 @@
+# LazCommandPalette
+
+LazCommandPalette is an IDE-Extension for Lazarus that resembles the command palette window of the [Atom](https://atom.io/) editor. If you press **Ctrl + Shift + P** a window appears where you can see a list of commands that can be filtered by a search and executed with the **Return** key or canceled with **Escape**.
+
+![video](./video/video.gif)
+
+Internally it works with the commands that can be assigned to the buttons of the toolbar. So if a commmand is not working it is most likely someone else`s fault :smiley:!
+
+# Licence
+
+Copyright (C) 2019 by Johannes Rosleff Sörensen
+
+This source is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 2 of the License, or (at your option)
+any later version.
+
+This code is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+A copy of the GNU General Public License is available on the World Wide Web
+at <http://www.gnu.org/copyleft/gpl.html>. You can also obtain it by writing
+to the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+Boston, MA 02110-1335, USA.
--- ulazcommandpalette.pas
+++ ulazcommandpalette.pas
@@ -20,8 +20,6 @@ type
     procedure FormKeyUp(Sender: TObject; var Key: Word; Shift: TShiftState);
     procedure FormShow(Sender: TObject);
     procedure SearchComEdAfterFilter(Sender: TObject);
-    procedure ViewComListDrawItem(Sender: TCustomListView; AItem: TListItem;
-      ARect: TRect; AState: TOwnerDrawState);
     procedure ViewComListSelectItem(Sender: TObject; Item: TListItem;
       Selected: Boolean);
   private
@@ -88,45 +86,9 @@ begin
     MenuItemCaption, nil, nil, ViewCommandPaletteCommand);
 
   CommandPalette := TCommandPalette.Create(Application);
-
-  // register dockable Window
-  {CommandPaletteCreator:=IDEWindowCreators.Add(
-    'CommandPalette',
-    @CreateCommandPalette, nil,
-    '100', '100', '300', '300'  // default place at left=100, top=100, right=300, bottom=300
-      // you can also define percentage values of screen or relative positions, see wiki
-    );}
-
 end;
 
 { TCommandPalette }
-
-procedure TCommandPalette.ViewComListDrawItem(Sender: TCustomListView;
-  AItem: TListItem; ARect: TRect; AState: TOwnerDrawState);
-var
-  Style: TTextStyle;
-begin
-  with ( Sender.Canvas ) do begin
-{    if ( bsPressed in AStates ) then begin
-      Brush.Color:= Blue900;
-      Font.Color:= White;
-    end else }if ( odSelected in AState ) then begin
-      Brush.Color:= $C06515;//Blue800;
-      Font.Color:= $FFFFFF;//White;
-    end else begin
-      Brush.Color:= $FFFFFF;//Blue700;
-      Font.Color:= $000000;//White;
-    end;
-
-    FillRect( ARect );
-    Style:= TextStyle;
-    Style.Alignment:= taLeftJustify;
-    Style.Layout:= tlCenter;
-    TListView( Sender ).SmallImages.Draw( Sender.Canvas, ARect.Left, ARect.Top, AItem.ImageIndex );
-    TextRect( ARect, 24, 0, AItem.Caption, Style );
-  end;
-
-end;
 
 procedure TCommandPalette.ViewComListSelectItem(Sender: TObject;
   Item: TListItem; Selected: Boolean);
@@ -192,15 +154,10 @@ function TCommandPalette.ShowModal: Integer;
 var
   i: Integer;
 begin
-  UpdateCommandPalette;
   SearchComEd.ResetFilter;
+  SearchComEd.Clear;
 
-  //Focus Edit
-  {for i:= 0 to SearchComEd.ControlCount - 1 do
-    if ( SearchComEd.Controls[ i ] is TEbEdit ) then begin
-      TEbEdit( SearchComEd.Controls[ i ]).Focused:= True;
-      break;
-    end;}
+  UpdateCommandPalette;
 
   Result:= inherited ShowModal;
 end;
